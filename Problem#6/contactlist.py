85e6c22a6033666886e625bc7b75bc2a66751dac
maurawahl <mjw4048@utulsa.edu>
2024-03-08 01:04:28 -0600

    all but lab 7 done

--- Problem#6/contactlist.py
+++ Problem#6/contactlist.py
@@ -3,21 +3,27 @@ Name: Maura Wahl
 Time: Thursdays at 2pm
 '''
 def process_user_contacts(user_input):
-    user_contacts = (user_input[0], user_input[1])
+    user_contacts = user_input.split(" ")
+    dictionary = {}
+    for x in range(0,len(user_contacts)):
+        new_contact = user_contacts[x].split(",")
+        dictionary[new_contact[0]] = new_contact[1]
+
+       # user_contacts.remove(user_contacts[x])
 
  
-    user_input = 
-    tokens = 
+    #user_input = 
+    #tokens = 
 
     # Put word pairs into a dictionary
     
     # Get contact name from input, output contact's phone number
     contact_name = input("Enter the contact name: ")
-    
+    print(dictionary[contact_name])
    
 if __name__ == '__main__':
     # Get input for word pairs
-    user_input = input("Enter word pairs (name, phone number): ").split(",")
+    user_input = input("Enter word pairs (name, phone number): ")
 
     # Call the function to process user contacts
     process_user_contacts(user_input)
